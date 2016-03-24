ed37b9b8f2541bed80459d8724518ef2b3cabee8
Petr Grabovoy <varren@gmail.com>
2016-03-24 18:05:01 +0300

    initial commit

---
+++ app/src/main/java/com/varren/animationdemo/MainActivity.java
@@ -0,0 +1,106 @@
+package com.varren.animationdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private List<String> mItems = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycleView);
+
+        //generating random data
+        for (int i = 0; i < 5000; i++) mItems.add("Cell " + i);
+
+        //recyclerView setup
+        recyclerView.setAdapter(new MyAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.getItemAnimator().setChangeDuration(500);
+    }
+
+    private void sort() {
+        for (int i = 0, j = mItems.size() - 1; i < j; i++, j--)
+            Collections.swap(mItems, i, j);
+
+        // Just small optimisation.
+        // can use adapter.notifyItemRangeChanged(0, mItems.size());
+
+        MyAdapter adapter = (MyAdapter) recyclerView.getAdapter();
+        LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        int firstVisible = layoutManager.findFirstVisibleItemPosition();
+        int lastVisible = layoutManager.findLastVisibleItemPosition();
+
+        adapter.notifyItemRangeChanged(firstVisible, lastVisible);
+    }
+
+    /************************************************************************************************
+     * simple RecyclerView Adapter
+     *************************************************************************************************/
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+            holder.titleView.setText(mItems.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView titleView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                titleView = (TextView) itemView.findViewById(R.id.titleTextView);
+            }
+        }
+    }
+
+    /************************************************************************************************
+     * MENU
+     *************************************************************************************************/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sort:
+                sort();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
